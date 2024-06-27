2bc937c8957457e69c26aa6ff4ca8556990fcc4c
DevSujal <nimjesujal2004@gmail.com>
2024-06-27 19:34:36 +0530

    "Added react-redux import and store usage to getAllData function"

--- src/components/getAllData.js
+++ src/components/getAllData.js
@@ -1,13 +1,18 @@
+import { useSelector } from "react-redux";
 import auth from "../app write services/auth.service";
 import database from "../app write services/database.service";
+import { store } from "../Store/store";
 
 export const getAllData = async () => {
-  const user = await auth.getCurrentUser();
+  const { authReducer } = store.getState();
+  if (!authReducer.status) {
+    const user = await auth.getCurrentUser();
 
-  if (!user) {
-    return null;
-  }
-  const notes = await database.getAllNotes(user);
+    if (!user) {
+      return null;
+    }
+    const notes = await database.getAllNotes(user);
 
-  return { user, notes: notes?.documents };
+    return { user, notes: notes?.documents };
+  } else return null;
 };
