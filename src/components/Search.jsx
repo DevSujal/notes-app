fc40581c553c8bb06f4873462e10e8b1683c221e
DevSujal <nimjesujal2004@gmail.com>
2024-07-03 14:07:37 +0530

    Refactor AddNotes and Search components to update styling and remove unnecessary code

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ function Search({className, setSearch, searchContent}) {
   return (
     <div className={`flex w-6/12 min-w-80 rounded overflow-hidden ${className}`}>
     <span className='text-2xl p-2 bg-slate-900'>🔍</span>
-      <Input value = {searchContent} onChange = {(e) => setSearch(e.target.value)} placeholder = "Search anything..." className = "w-full bg-slate-900 hover:bg-slate-900 text-white rounded-none"/>
+      <Input value = {searchContent} onChange = {(e) => setSearch(e.target.value)} placeholder = "Search anything..." className = "w-full bg-slate-900 text-white rounded-none"/>
     </div>
   )
 }
