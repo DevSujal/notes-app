503e799e701911809a30e71d98b7275d1f593cdb
DevSujal <nimjesujal2004@gmail.com>
2024-07-04 09:44:46 +0530

    Refactor AddNotes component to update styling and use consistent color scheme

--- src/components/AddNotes.jsx
+++ src/components/AddNotes.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function AddNotes() {
   return (
-    <Link to="/new" className='w-20 h-20 rounded-full text-center fixed bottom-6 bg-black/50 font-bold'>
+    <Link to="/new" className='w-20 h-20 rounded-full text-center sm:pt-0 pt-2 fixed bottom-6 bg-black/30 font-bold'>
       <span  className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 sm:text-7xl text-6xl z-10 ">+</span>
     </Link>
   )
